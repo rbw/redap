93266b348e057a2c35dc380cd5c4ca87ef1799dc
Robert Wikman <rbw@vault13.org>
2018-06-23 18:58:45 +0200

    Reworked base CRUD

--- lapdance/services/base.py
+++ lapdance/services/base.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import current_app as app
+from ldap3 import MODIFY_REPLACE
 from lapdance.core import ldap
 from lapdance.exceptions import LapdanceError
 from lapdance.utils import props_to_str
-
-VENDOR_MICROSOFT = 'MICRO$OFT'
-VENDOR_UNKNOWN = 'UNKNOWN'
 
 
 class ResponseHandler(object):
@@ -44,6 +42,10 @@ class Service(object):
         return app.config[self.__config_name__]
 
     @property
+    def fields(self):
+        return self.config['fields']
+
+    @property
     def dirtype(self):
         return app.config['LAPDANCE_LDAP_DIRTYPE']
 
@@ -57,22 +59,51 @@ class Service(object):
             raise LapdanceError(message='Operation not compatible with this directory server', status_code=500)
 
     def _get_matching(self, query_filter=None, raise_on_empty=False):
-        """Perform LDAP query
-
-        :param query_filter: Query filter string
-        :param raise_on_empty: Raise exception if no records returned
-        :return: ResponseHandler object
-        """
-
         return ResponseHandler(
             self.model.query.filter(query_filter),
             self.config,
             raise_on_empty=raise_on_empty
         )
 
-    def get_many(self, **kwargs):
-        """Returns a list of entries matching the query params"""
+    def _get_entry_dn(self, id_value):
+        return "{0}={1},{2},{3}".format(self.fields['id']['ldap_name'],
+                                        id_value,
+                                        self.config['relative_dn'],
+                                        self.model.base_dn)
 
+    def _create_payload(self, params, operation=None):
+        """Create LDAP payload from dict"""
+
+        payload = {}
+
+        for name, field in self.fields.items():
+            # Look for missing props if operation is None (add) and set defaults.
+            # No need to check for constraints as the input has been validated already.
+            if name not in params:
+                if 'default' in field and operation is None:
+                    value = field['default']
+                else:
+                    continue
+            else:
+                value = params[name]
+
+            if operation:
+                value = [(operation, value, )]
+
+            payload[field['ldap_name']] = value
+
+        return payload
+
+    def _modify(self, id_value, params):
+        self.conn.modify(dn=self._get_entry_dn(id_value),
+                         changes=self._create_payload(params, MODIFY_REPLACE))
+
+    def _add(self, params):
+        self.conn.add(dn=self._get_entry_dn(params['id']),
+                      object_class=self.config['classes'],
+                      attributes=self._create_payload(params))
+
+    def get_many(self, **kwargs):
         as_dict = kwargs.pop('as_dict', True)
         return self._get_matching(
             query_filter=kwargs.pop('filter', ''),
@@ -80,30 +111,17 @@ class Service(object):
         ).result(as_dict=as_dict) or []
 
     def get_one(self, id_value, raise_on_empty=True, as_dict=False):
-        """Returns a single entry"""
-
         id_field = self.config['fields']['id']
         return self._get_matching(
             query_filter='({0}={1})'.format(id_field['ldap_name'], id_value),
             raise_on_empty=raise_on_empty,
         ).result(as_dict=as_dict)[0]
 
-    def update(self, id_value, payload):
-        """Updates an entry"""
+    def update(self, id_value, params):
+        self._modify(id_value, params)
 
-        obj = self.get_one(id_value)
-        for (k, v) in payload.items():
-            setattr(obj, k, v)
-
-        obj.save()
-
-    def create(self, payload):
-        fields = self.config['fields']
-
-        self.conn.add(dn="{0}={1},{2}".format(fields['id']['ldap_name'], payload['id'], self.model.base_dn),
-                      object_class=self.config['classes'],
-                      attributes={fields[k]['ldap_name']: v for k, v in payload.items()})
+    def create(self, params):
+        self._add(params)
 
     def delete(self, query_id):
-        obj = self.get_one(query_id)
-        self.conn.delete(obj.dn)
+        self.conn.delete(self._get_entry_dn(query_id))
