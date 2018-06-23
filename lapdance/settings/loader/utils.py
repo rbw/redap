453ddce8fca2c0bbd460dff6ac53aade4703bc4c
Robert Wikman <rbw@vault13.org>
2018-06-23 06:32:41 +0200

    Adds _apply_directory_defaults()

--- lapdance/settings/loader/utils.py
+++ lapdance/settings/loader/utils.py
@@ -6,10 +6,15 @@ from cerberus import Validator
 from lapdance.exceptions import InvalidConfiguration
 
 
-def load_doc(file, schema=None):
+def load_doc(file, schema=None, defaults=None):
     with open(file, 'r') as stream:
         try:
-            doc = yaml.load(stream)
+            doc = yaml.load(stream) or {}
+
+            if defaults:
+                # Apply defaults for the selected profile
+                doc = _apply_directory_defaults(doc or {}, defaults)
+
             v = Validator(schema)
             v.allow_unknown = True
             v.validate(doc, schema)
@@ -20,3 +25,19 @@ def load_doc(file, schema=None):
         except yaml.YAMLError as exception:
             raise exception
 
+
+def _apply_directory_defaults(settings, defaults):
+    def _is_empty(value):
+        if isinstance(value, str) and not value:
+            return True
+        elif isinstance(value, list) and len(value) == 0:
+            return True
+
+        return False
+
+    for k, v in defaults.items():
+        # Set value from profile if empty
+        if k not in settings or _is_empty(settings[k]):
+            settings[k] = v
+
+    return settings
