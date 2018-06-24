c6454f7d8918d034b119ca77248b1b2963ef1800
Robert Wikman <rbw@vault13.org>
2018-06-24 03:31:01 +0200

    Added default AD profile

--- lapdance/settings/loader/profiles/ad.py
+++ lapdance/settings/loader/profiles/ad.py
@@ -4,13 +4,95 @@ defaults_ad = {
     'group': {
         'classes': [
             'top',
-            'group'
-        ]
+            'group',
+        ],
+        'hidden_fields': [
+            'distinguished_name'
+        ],
+        'required_fields': [
+            'id', 'description'
+        ],
+        'fields': {
+            'id': {
+                'type': 'string',
+                'ref': 'sAMAccountName'
+            },
+            'name': {
+                'type': 'string',
+                'ref': 'cn'
+            },
+            'created_at': {
+                'type': 'string',
+                'ref': 'whenCreated'
+            },
+            'updated_at': {
+                'type': 'string',
+                'ref': 'whenChanged'
+            },
+            'distinguished_name': {
+                'type': 'string',
+                'ref': 'distinguishedName'
+            },
+            'description': {
+                'type': 'string',
+                'ref': 'description'
+            },
+        }
     },
     'user': {
         'classes': [
             'top',
+            'person',
+            'organizationalPerson',
             'user'
-        ]
+        ],
+        'hidden_fields': [
+            'distinguished_name'
+        ],
+        'required_fields': [
+            'id', 'email', 'name', 'last_name', 'first_name'
+        ],
+        'fields': {
+            'id': {
+                'type': 'string',
+                'ref': 'sAMAccountName'
+            },
+            'name': {
+                'type': 'string',
+                'ref': 'cn'
+            },
+            'email': {
+                'type': 'string',
+                'ref': 'mail'
+            },
+            'distinguished_name': {
+                'type': 'string',
+                'ref': 'distinguishedName'
+            },
+            'last_name': {
+                'type': 'string',
+                'ref': 'sn'
+            },
+            'first_name': {
+                'type': 'string',
+                'ref': 'givenName'
+            },
+            'last_logon': {
+                'type': 'integer',
+                'ref': 'lastLogon',
+            },
+            'logon_count': {
+                'type': 'integer',
+                'ref': 'logonCount',
+            },
+            'created_at': {
+                'type': 'string',
+                'ref': 'whenCreated',
+            },
+            'updated_at': {
+                'type': 'string',
+                'ref': 'whenChanged',
+            }
+        }
     }
 }
