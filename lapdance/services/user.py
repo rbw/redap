173fcebcaa19f584fe9855c887682198320df55f
Robert Wikman <rbw@vault13.org>
2018-06-24 03:29:09 +0200

    Reworked user service: - added authenticate() - set_password() now compatible with all - ldap_name -> ref - use dirtype constants

--- lapdance/services/user.py
+++ lapdance/services/user.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from ldap3 import MODIFY_REPLACE
+from ldap3.core.exceptions import LDAPBindError
 from lapdance.models import LDAPUser
-from .base import Service
+from lapdance.core import ldap
+from .base import Service, ACTIVE_DIRECTORY
 
 # UserAccountControl flags
 UAC_PW_NEVER_EXPIRES = 66048
@@ -15,12 +17,12 @@ class UserService(Service):
     __config_name__ = 'LAPDANCE_LDAP_USER'
 
     def _set_account_control(self, user_id, flag):
-        self._raise_if_incompatible_with('ad')
+        self._raise_if_incompatible_with(ACTIVE_DIRECTORY)
         user = self.get_one(user_id)
         user.userAccountControl = [(MODIFY_REPLACE, [flag])]
 
     def is_member_of(self, user_dn, group_dn):
-        id_attr = self.config['fields']['id']['ldap_name']
+        id_attr = self.config['fields']['id']['ref']
         return len(self.get_many(filter='({0}={1})(memberOf={2})'.format(id_attr, user_dn, group_dn))) > 0
 
     @property
@@ -31,21 +33,33 @@ class UserService(Service):
         return groups
 
     def get_groups(self, user_id, include_nested=False, **kwargs):
-        """Imports groups service and obtains a list of groups for the user"""
-
         user_dn = self.get_one(user_id).dn
 
         if str(include_nested).lower() in [str(1), 'true']:
-            self._raise_if_incompatible_with('ad')
+            self._raise_if_incompatible_with(ACTIVE_DIRECTORY)
             kwargs['filter'] = '(member:1.2.840.113556.1.4.1941:={0})'.format(user_dn)
         else:
             kwargs['filter'] = '(member={0})'.format(user_dn)
 
         return self._groups.get_many(**kwargs)
 
+    def authenticate(self, username, password):
+        user_dn = self.get_one(username).dn
+
+        try:
+            conn = ldap.connect(user_dn, password)
+            conn.unbind()
+            return True
+        except LDAPBindError:
+            return False
+
     def set_password(self, user_id, **kwargs):
         user = self.get_one(user_id)
-        self._microsoft_ext.modify_password(user.dn, **kwargs)
+
+        if self.dirtype == ACTIVE_DIRECTORY:
+            self._microsoft_ext.modify_password(user.dn, **kwargs)
+        else:
+            self.conn.extend.standard.modify_password(user.dn, **kwargs)
 
     def unlock(self, user_id):
         self._microsoft_ext.unlock_account(self.get_one(user_id).dn)
